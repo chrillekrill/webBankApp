b43f5a750aa7021f632bc0e069b2193784431dc4
Christoffer K <henricson21@hotmail.com>
2022-04-28 15:03:00 +0200

    stuff

--- BankStartWeb/Pages/Accounts/SendTransactionView.cshtml.cs
+++ BankStartWeb/Pages/Accounts/SendTransactionView.cshtml.cs
@@ -1,5 +1,6 @@
 using BankAppWeb.Services;
 using BankStartWeb.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
